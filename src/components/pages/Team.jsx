826536c3ec4ad36baad04b06db2e8d6a43000ff0
Joshua Zega <hedywidja321@gmail.com>
2024-04-27 13:01:28 +0700

    Creating responsive navbar + routing

---
+++ src/components/pages/Team.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Team = () => {
+    return (
+        <div>
+            <div className="lorem">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, placeat odio. Autem provident, fugit veniam reiciendis veritatis incidunt saepe dicta modi, vero mollitia unde obcaecati! Earum quia a et inventore.
+                <br />
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio in nostrum qui? At excepturi enim, minus, similique, ullam sunt voluptate molestias accusantium eius dolorem quasi tenetur. Amet obcaecati debitis quos.
+                <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ipsum harum dolor, iusto illum eos, commodi reprehenderit distinctio veniam architecto tempore dolorum soluta accusamus ut fugit animi, id voluptate ipsa!
+                <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis odit incidunt veniam exercitationem doloremque nesciunt illum aliquid placeat deleniti minima praesentium corporis nam assumenda modi ullam similique, optio alias accusamus?
+            </div>
+        </div>
+    )
+}
+
+export default Team
